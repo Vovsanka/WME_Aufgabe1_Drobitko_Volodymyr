e7628d6f3890a1c474b5d429fefae7cdae59c0e2
Vovsanka <drobitkovolodya@gmail.com>
2023-10-24 01:21:28 +0200

    load table via js

--- main.js
+++ main.js
@@ -3,7 +3,25 @@ import tableData from "/dataset/dataset.js";
 const headers = tableData.headers
 const dataset = tableData.dataset
 
-console.log(headers)
-console.log(dataset)
+// create a table contents
+let table = document.getElementById('data-table')
+let thead = table.querySelector('thead')
+let tbody = table.querySelector('tbody')
 
-// create a table
+// init table headers
+let theadTr = thead.insertAdjacentElement('beforeend', document.createElement('tr'))
+console.log(theadTr)
+for (const header of headers) {
+    theadTr.insertAdjacentHTML('beforeend', `<th>${header}</th>`)
+}
+
+// init table body
+for (const item of dataset) {
+    let tr = document.createElement('tr')
+    for (const header of headers) {
+        tr.insertAdjacentHTML('beforeend',  `<td>${item[header]}</td>`)
+    }
+    tbody.insertAdjacentElement('beforeend', tr)
+}
+
+console.log(table)
