f3e4956165e6a1028559cd1f49d8367fa4718f2b
Vovsanka <drobitkovolodya@gmail.com>
2023-10-24 01:38:51 +0200

    table alt gray background

--- main.js
+++ main.js
@@ -3,25 +3,32 @@ import tableData from "/dataset/dataset.js";
 const headers = tableData.headers
 const dataset = tableData.dataset
 
-// create a table contents
-let table = document.getElementById('data-table')
-let thead = table.querySelector('thead')
-let tbody = table.querySelector('tbody')
+const fillTable = () => {
+    // create a table contents
+    let table = document.getElementById('data-table')
+    let thead = table.querySelector('thead')
+    let tbody = table.querySelector('tbody')
 
-// init table headers
-let theadTr = thead.insertAdjacentElement('beforeend', document.createElement('tr'))
-console.log(theadTr)
-for (const header of headers) {
-    theadTr.insertAdjacentHTML('beforeend', `<th>${header}</th>`)
-}
-
-// init table body
-for (const item of dataset) {
-    let tr = document.createElement('tr')
+    // init table headers
+    let theadTr = thead.insertAdjacentElement('beforeend', document.createElement('tr'))
+    console.log(theadTr)
     for (const header of headers) {
-        tr.insertAdjacentHTML('beforeend',  `<td>${item[header]}</td>`)
+        theadTr.insertAdjacentHTML('beforeend', `<th>${header}</th>`)
     }
-    tbody.insertAdjacentElement('beforeend', tr)
+
+    // init table body
+    for (const [index, item] of dataset.entries()) {
+        let tr = document.createElement('tr')
+        if (index % 2 === 1)
+            tr.style.backgroundColor = '#e0e0e0'
+        for (const header of headers) {
+            tr.insertAdjacentHTML('beforeend',  `<td>${item[header]}</td>`)
+        }
+        tbody.insertAdjacentElement('beforeend', tr)
+    }
 }
 
-console.log(table)
+
+// main
+fillTable()
+
